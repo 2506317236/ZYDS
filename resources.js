4be7065f209cbe9e79de2aaaf8e837bc29abea72
2506317236 <110875382+2506317236@users.noreply.github.com>
2022-11-26 13:35:21 +0800

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,22 @@
+let : {
+  n = 8;//视频个数
+  video = [
+  {vsrc:"http://suol.cc/yzAJm",xtext:"「特」ATRI动画化非漫"},
+  {vsrc:"http://suol.cc/hmlWs0",xtext:"So Far Away"},
+  {vsrc:"http://suol.cc/q4gOg0",xtext:"Falling Again"},
+  {vsrc:"http://suol.cc/F91Fz",xtext:"Come Back Down"},
+  {vsrc:"http://suol.cc/Q8BhN1",xtext:"洁白的婚纱",author:"https://v.kuaishou.com/AD5FvV"},
+  {vsrc:"http://suol.cc/I6bgW1",xtext:"Broken From The Start",author:"https://v.kuaishou.com/ynnA5E"},
+  {vsrc:"http://suol.cc/CAAod2",xtext:"Fool For You",author:"https://v.kuaishou.com/yLdDza"},
+  {vsrc:"http://suol.cc/fvRCt0",xtext:"Catch My Breath",author:"https://v.kuaishou.com/AiFOSH"},
+  {vsrc:"http://suol.cc/kgu1s2",xtext:"Umbrella",author:"https://v.kuaishou.com/wQiN88"},
+  ]
+  mangeng = [
+  {vsrc:"",xtext:"",author:""},
+  {vsrc:"",xtext:"",author:""},
+  {vsrc:"",xtext:"",author:""},
+  ]
+  dongman = [
+  {vsrc:"http://suol.cc/riVve1",xtext:"「新海诚新作」铃芽户缔 宣传片",author:"https://v.kuaishou.com/zBXFjp"},
+  ]
+}
